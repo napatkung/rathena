5395cf0b7b6e91e120390ecddc816df35e2b64fc
Cydh Ramdh <house.bad@gmail.com>
2015-03-11 23:38:15 +0700

    Change `amount` type to SMALLINT(5) UNSIGNED
    Signed-off-by: Cydh Ramdh <house.bad@gmail.com>

--- sql-files/upgrades/upgrade_20150305_market.sql
+++ sql-files/upgrades/upgrade_20150305_market.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS `market` (
   `name` varchar(32) NOT NULL DEFAULT '',
   `nameid` SMALLINT(5) UNSIGNED NOT NULL,
-  `price` INT(11) UNSIGNED NOT NULL,
+  `price` SMALLINT(5) UNSIGNED NOT NULL,
   `amount` INT(11) UNSIGNED NOT NULL,
   `flag` TINYINT(2) UNSIGNED NOT NULL DEFAULT '0',
   PRIMARY KEY  (`name`,`nameid`)
